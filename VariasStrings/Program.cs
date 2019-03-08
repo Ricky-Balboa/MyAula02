686c1a689dfe385085dfd4d8e131e520600a0126
The Very Cool School Pc <ddd.com>
2019-03-08 10:56:36 +0000

    comentação do codigo

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,51 +6,51 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            double xx = 0.12345;
-            int ii = 18;
+            double xx = 0.12345;//Variaveis Ex 3
+            int ii = 18;//Variaveis Ex 3
 
-            int x = 2;
-            int y = 5;
-            string a = " Isto é uma string ";
-            string b = @" Isto é uma string verbatim/literal";
-            string c = " Unicode -> \u0081";
-            string d = " Unicode -> \u03A6";
-            string e = " Um \t (tab) e uma  \\ (barra) ";
-            string Break = "|------------------------------------------------------|";
-            string s = String.Format(" Ola {0} No. {1}!", "mundo", 3);
-            string f = String.Format(" {0} mais {0} é igual a {1}", "Dois", "Quatro");
-            string g = " sou" + "uma string" + " concatenada ";
-            g += "(sou sim senhor)";//texto do final da string g
-            string h = $" {x} mais {y} é igual {x+y} feito por interpolação";
+            int x = 2;//Variaveis
+            int y = 5;//Variaveis
+            string a = " Isto é uma string ";//Exercicio 1 
+            string b = @" Isto é uma string verbatim/literal";// Exercicio 1
+            string c = " Unicode -> \u0081";//Exercicio 1
+            string d = " Unicode -> \u03A6";//Exercicio 1
+            string e = " Um \t (tab) e uma  \\ (barra) ";//Exercicio 1 
+            string Break = "|------------------------------------------------------|";//Intervalo 
+            string s = String.Format(" Ola {0} No. {1}!", "mundo", 3);//Exercicio 2 
+            string f = String.Format(" {0} mais {0} é igual a {1}", "Dois", "Quatro");//Exercicio 2 
+            string g = " sou" + "uma string" + " concatenada ";//Exercicio 2 
+            g += "(sou sim senhor)";//texto do final da string g Exercicio2
+            string h = $" {x} mais {y} é igual {x+y} feito por interpolação";//Exercicio 2
            
 
-            Console.WriteLine(a);
+            Console.WriteLine(a);//
             Console.WriteLine(Break);
-            Console.WriteLine(b);
+            Console.WriteLine(b);//
             Console.WriteLine(Break);
-            Console.WriteLine(c);
+            Console.WriteLine(c);//
             Console.WriteLine(Break);
-            Console.WriteLine(d);
+            Console.WriteLine(d);//
             Console.WriteLine(Break);
-            Console.WriteLine(e);
+            Console.WriteLine(e);//
             Console.WriteLine(Break);
-            Console.WriteLine(s);
+            Console.WriteLine(s);//
             Console.WriteLine(Break);
-            Console.WriteLine(f);
+            Console.WriteLine(f);//
             Console.WriteLine(Break);
-            Console.WriteLine(g);
+            Console.WriteLine(g);//
             Console.WriteLine(Break);
-            Console.WriteLine(h);
+            Console.WriteLine(h);//
             Console.WriteLine(Break);
-            Console.WriteLine(@" Sou mais uma string autista em verbatim :D");
+            Console.WriteLine(@" Sou mais uma string autista em verbatim :D");//Exercicio 2
             Console.WriteLine(Break);
-            Console.WriteLine($" Exercicio 3 -> {xx:f2}");
+            Console.WriteLine($" Exercicio 3 -> {xx:f2}");//Exercicio 3
             Console.WriteLine(Break);
-            Console.WriteLine($" Exercicio 3 -> {xx:p1}");
+            Console.WriteLine($" Exercicio 3 -> {xx:p1}");//Exercicio 3 
             Console.WriteLine(Break);
-            Console.WriteLine($" Exercicio 3 -> {ii:x}");
+            Console.WriteLine($" Exercicio 3 -> {ii:x}");//Exercicio 3 
             Console.WriteLine(Break);
-            Console.WriteLine($" Exercicio 3 -> {ii:c}");
+            Console.WriteLine($" Exercicio 3 -> {ii:c}");//Exercicio 3 
             Console.WriteLine(Break);
             Console.WriteLine(" Enter Para Fechar!");
             Console.ReadKey();
