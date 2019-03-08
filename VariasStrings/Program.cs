3ec255d33178337fefb84769053e776f36d1f3de
The Very Cool School Pc <ddd.com>
2019-03-08 09:20:42 +0000

    Primeiro Commit

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,7 +6,23 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = "isto é uma string ";
+            string b = @"Isto é uma string verbatim/literal";
+            string c = " Unicode -> \u0081";
+            string d = " Unicode -> \u03A6";
+            string Break = "------------------------------------------------------";
+
+            Console.WriteLine(a);
+            Console.WriteLine(Break);
+            Console.WriteLine(b);
+            Console.WriteLine(Break);
+            Console.WriteLine(c);
+            Console.WriteLine(Break);
+            Console.WriteLine(d);
+            Console.WriteLine(Break);
+            Console.WriteLine("Enter Para Fechar.");
+            Console.ReadKey();
+
         }
     }
 }
