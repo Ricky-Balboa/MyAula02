710e1c1900aaccc42fc65a47713a163089fe325f
The Very Cool School Pc <ddd.com>
2019-03-08 12:18:22 +0000

    retoques no codigo

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -11,13 +11,15 @@ namespace Cilindro
             Console.WriteLine("Insira uma Altura");//Pedir Altura 
             string a = Console.ReadLine();//Receber input 
             int A = Convert.ToInt32(a);//Converter para Inteiro o input
+            Console.WriteLine("-----------------------------------------------");
             Console.WriteLine("Insira um Raio");//Pedir Raio
             string r = Console.ReadLine();//Receber Input
             int R = Convert.ToInt32(r);//Converter para inteiro o input
             double V = Math.PI * R * R * A;//Calcular o Volume
             double D = 2*Math.PI*R*(R+A);//Calcular a Area
-            string v = $"O Volume do Cilindro é: V = \u03C0r^2A = {V}";//String do resultado
-            string d = $"A Area do Cilindro é: A = 2\u03C0r(r+A) = {D} ";//String do resultado
+            Console.WriteLine("-----------------------------------------------");
+            string v = $"O Volume do Cilindro é: V = \u03C0r^2A = {V:f3}";//String do resultado
+            string d = $"A Area do Cilindro é: A = 2\u03C0r(r+A) = {D:f3} ";//String do resultado
             Console.WriteLine(v);//Apresentar o resultado
             Console.WriteLine(d);//Apresentar o resultado
             Console.ReadKey();
