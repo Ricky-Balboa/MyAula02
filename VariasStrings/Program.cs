1c60a8029630799952956a4c5f3c809438796c85
The Very Cool School Pc <ddd.com>
2019-03-08 09:25:22 +0000

    Adicionar mais strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,10 +6,11 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            string a = "isto é uma string ";
+            string a = "Isto é uma string ";
             string b = @"Isto é uma string verbatim/literal";
             string c = " Unicode -> \u0081";
             string d = " Unicode -> \u03A6";
+            string e = "Um \t (tab) e uma  \\ (barra) ";
             string Break = "------------------------------------------------------";
 
             Console.WriteLine(a);
@@ -19,6 +20,8 @@ namespace VariasStrings
             Console.WriteLine(c);
             Console.WriteLine(Break);
             Console.WriteLine(d);
+            Console.WriteLine(Break);
+            Console.WriteLine(e);
             Console.WriteLine(Break);
             Console.WriteLine("Enter Para Fechar.");
             Console.ReadKey();
