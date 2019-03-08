2e4349c51e329da3464190d31c6878c8fbaed668
The Very Cool School Pc <ddd.com>
2019-03-08 12:10:10 +0000

    Exercicio 5 e 6 resolvidos

---
+++ Cilindro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Insira uma Altura");//Pedir Altura 
+            string a = Console.ReadLine();//Receber input 
+            int A = Convert.ToInt32(a);//Converter para Inteiro o input
+            Console.WriteLine("Insira um Raio");//Pedir Raio
+            string r = Console.ReadLine();//Receber Input
+            int R = Convert.ToInt32(r);//Converter para inteiro o input
+            double V = Math.PI * R * R * A;//Calcular o Volume
+            double D = 2*Math.PI*R*(R+A);//Calcular a Area
+            string v = $"O Volume do Cilindro é: V = \u03C0r^2A = {V}";//String do resultado
+            string d = $"A Area do Cilindro é: A = 2\u03C0r(r+A) = {D} ";//String do resultado
+            Console.WriteLine(v);//Apresentar o resultado
+            Console.WriteLine(d);//Apresentar o resultado
+            Console.ReadKey();
+
+        }
+    }
+}
